006ddff88cbd457336e5662adaa9035cf6f41e8a
mohanpillimitla <mohanpillimitla143@gmail.com>
2020-06-14 11:54:09 +0530

    1 file modified

--- components/UserForm.jsx
+++ components/UserForm.jsx
@@ -3,6 +3,61 @@ import React, { Component } from 'react';
 class UserForm extends Component
 {
 
+  state={
+
+    step:1,
+    firstname:'',
+    email:'',
+    lastname:'',
+    bio:'',
+    occupation:'',
+
+
+
+  }
+  //handlechange
+
+
+  handleChange=input=>(event)=>{
+
+    this.setState({[input]:event.target.value})
+
+  }
+
+
+
+
+
+
+  //nextstep
+
+  nextstep  =()=>
+  {
+    const {step}=this.state;
+    this.setState({
+      step:step+1
+
+    })
+
+
+
+
+  }
+//nexstep
+
+   nextstep  =()=>
+  {
+    const {step}=this.state;
+    this.setState({
+      step:step-1
+
+    })
+
+
+
+
+  }
+
    render()
    {
      return (
