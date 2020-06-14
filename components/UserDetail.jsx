4c17958e8fccbe2ca855d4de1dd555776352d54a
mohanpillimitla <mohanpillimitla143@gmail.com>
2020-06-14 14:48:21 +0530

    2 files modified

--- components/UserDetail.jsx
+++ components/UserDetail.jsx
@@ -13,8 +13,13 @@ class UserDetail extends Component
    render()
    {
       return(
+              <MuiThemeProvider>
+              <div>
 
-                ''
+                <TextField/>
+             </div>
+              </MuiThemeProvider>
+
 
       )
 
